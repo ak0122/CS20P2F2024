c37da33616aa032f78218e9ff298d18f01f19d4a
1100102072 <1100102072@L815WD220009.edu.cbe.ab.ca>
2024-09-17 10:12:25 -0600

    10:11 9/17/24

--- chapter3/src/Mastery/DigitsE6.java
+++ chapter3/src/Mastery/DigitsE6.java
@@ -1,17 +1,65 @@
 package Mastery;
 
+import java.util.Scanner;
+
 public class DigitsE6 {
+	
+	/*
+
+	Program: DigitsE6.java          Last Date of this Revision: 9/17/24
+
+	Purpose: An application that uses nested for loops to output a table that diplays number in 
+	 multiples of ten.
+
+	Author: Aryan Kapoor, 
+	School: CHHS
+	Course: Computer Programming 20
+	
+	*/
+
 
 	public static void main(String[] args)
 	{
 		//Declaration
-		int n; h ;t ;o ; 
+		int n, h, t, o ; 
 		
 		//Prep for User Input
 		
 		Scanner UserInput = new Scanner(System.in);
 		
-		//Ask for 3-digit number from User
+		//Ask for 3-digit number from User and receive the input
+		System.out.print("Please enter a three digit number: ") ;
+		
+		n = UserInput.nextInt();
+		
+		//Perform int division to get the hundreds value
+		
+		h = n/100 ; 
+		
+		//Calculate the tens value
+		
+		t = (n-h*100)/10 ;
+		
+		// perform modulo division on n to get the ones value
+		
+		o = n%10 ;
+		
+		//Print the Output
+		
+		System.out.print("The value of the hundreds is " + h +", the value of the tens is " + t + ", and the value of the ones is " + o + ".");
 		
 		}
 }
+
+/* Screen Dump
+ * 
+Please enter a three digit number: 123
+The value of the hundreds is 1, the value of the tens is 2, and the value of the ones is 3.
+
+Please enter a three digit number: 763
+The value of the hundreds is 7, the value of the tens is 6, and the value of the ones is 3.
+
+
+
+ */
+
