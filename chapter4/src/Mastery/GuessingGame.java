048eef3644ce88182a289dbde666278225de72e1
1100102072 <1100102072@L815WD220009.edu.cbe.ab.ca>
2024-10-01 10:18:21 -0600

    10:17

---
+++ chapter4/src/Mastery/GuessingGame.java
@@ -0,0 +1,64 @@
+
+/*
+
+Program: GuessingGame.java          Last Date of this Revision: September 29, 2024
+
+Purpose: An application that uses if-else statements to create a game where a player has to guess the correct random number between 1 and 20.
+
+Author: Aryan Kapoor, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/package Mastery;
+import java.util.*;
+
+public class GuessingGame {
+
+	public static void main(String[] args)
+	{
+		//Declaration
+		
+		int userguess,cpuguess;
+		
+		//Prepare for user input and set up randomization
+		Scanner UserInput = new Scanner(System.in);
+		Random rndm = new Random();
+				
+		//Prompt user to guess an integer b/w 1-20
+		System.out.print("Enter a number between 1 and 20: ");
+		userguess = UserInput.nextInt();
+		
+		//Computer chooses a number
+		
+		cpuguess = rndm.nextInt(20)+1;
+		
+		//Print Statement to show value of user and computer guess
+		System.out.print("\nComputer's Guess: "+cpuguess);
+		System.out.print("\nPlayer's Guess: "+userguess);
+		
+		//If statement to compare user to cpu guess
+		
+		if (cpuguess == userguess) {
+			System.out.print("\nYou Won!");
+		}
+		else {
+			System.out.print("\nBetter luck next time.");
+		}
+	}
+}
+
+/* Test case 1:
+Enter a number between 1 and 20: 7
+
+Computer's Guess: 7
+Player's Guess: 7
+You Won!
+
+Case 2:
+Enter a number between 1 and 20: 15
+
+Computer's Guess: 6
+Player's Guess: 15
+Better luck next time.
+*/
--- chapter4/src/Mastery/MathTutor.java
+++ chapter4/src/Mastery/MathTutor.java
@@ -1,70 +1,80 @@
-package Mastery;
+/*
 
+Program: MathTutor.java          Last Date of this Revision: September 27, 2024
+
+Purpose: An application that uses if-else statements and random numbers to give the user a random math equation and tell them if they got it right or wrong.
+
+Author: Aryan Kapoor, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+package Mastery;
 import java.text.DecimalFormat;
 import java.util.Random;
 import java.util.Scanner;
-
 public class MathTutor {
 
-	public static void main(String[] args)
-	{
-		//declaration
-		int n_1,n_2,op,ans1,ans2,ans3;
-		String ans4;
-		
-		//setting up user input and random number
-		Random random = new Random();
-		Scanner UserInput = new Scanner(System.in);
-		
-		op = random.nextInt(4);
-		n_1 = random.nextInt(10)+1;
-		n_2 = random.nextInt(10)+1;
-		
-		DecimalFormat df = new DecimalFormat("0.00");
-		
-		//addition case --> op = 0 corresponds to add
-		if (op == 0) {
-			System.out.print("What is " +n_1+"+"+n_2+"? ");
-			ans1 = UserInput.nextInt();
-			if (ans1 == n_1 +n_2) {
-				System.out.print("You got the answer RIGHT! Good Job.");
-			}
-				else {
-					System.out.print("You got the answer WRONG.");
-				}}
+		public static void main(String[] args)
+		{
+			//declaration
+			int n_1,n_2,op,ans;
+			String ans4;
 			
-			//Subtraction case --> op == 1 is subtract
-			else if (op == 1) {
-				System.out.print("What is " +n_1+"-"+n_2+"? ");
-				ans2 = UserInput.nextInt();
-				if (ans2 == n_1 - n_2) {
-					System.out.print("You got the answer RIGHT! Good Job.");
-				}
-					else {
-						System.out.print("You got the answer WRONG.");
-					}
-				}
-		//Multiplication case --> op == 2 is subtract
-			else if (op == 2) {
-				System.out.print("What is " +n_1+"*"+n_2+"? ");
-				ans3 = UserInput.nextInt();
-				if (ans3 == n_1*n_2) {
+			//setting up user input and random number
+			Random random = new Random();
+			Scanner UserInput = new Scanner(System.in);
+			
+			op = random.nextInt(4);
+			n_1 = random.nextInt(10)+1;
+			n_2 = random.nextInt(10)+1;
+			
+			DecimalFormat df = new DecimalFormat("0.00");
+			
+			//addition case --> op = 0 corresponds to add
+			if (op == 0) {
+				System.out.print("What is " +n_1+"+"+n_2+"? ");
+				ans = UserInput.nextInt();
+				if (ans == n_1 +n_2) {
 					System.out.print("You got the answer RIGHT! Good Job.");
 				}
 					else {
 						System.out.print("You got the answer WRONG.");
 					}}
-		//Division case --> op == 3 is divide
-				else if (op == 3) {
-					System.out.print("What is " +n_1+"/"+n_2+" to 2 decimal places? ");
-					ans4 = UserInput.next();
-					if (ans4 == df.format((double)n_1/(double)n_2)) {
+				
+				//Subtraction case --> op == 1 is subtract
+				else if (op == 1) {
+					System.out.print("What is " +n_1+"-"+n_2+"? ");
+					ans = UserInput.nextInt();
+					if (ans == n_1 - n_2) {
 						System.out.print("You got the answer RIGHT! Good Job.");
 					}
 						else {
 							System.out.print("You got the answer WRONG.");
 						}
-			}	
+					}
+			//Multiplication case --> op == 2 is multiply
+				else if (op == 2) {
+					System.out.print("What is " +n_1+"*"+n_2+"? ");
+					ans = UserInput.nextInt();
+					if (ans == n_1*n_2) {
+						System.out.print("You got the answer RIGHT! Good Job.");
+					}
+						else {
+							System.out.print("You got the answer WRONG.");
+						}}
+			//Division case --> op == 3 is divide
+					else if (op == 3) {
+						System.out.print("What is " +n_1+"/"+n_2+" to 2 decimal places? ");
+						ans4 = UserInput.next();
+						if (ans4.equals(df.format((double)n_1/(double)n_2))) {
+							System.out.print("You got the answer RIGHT! Good Job.");
+						}
+							else {
+								System.out.print("You got the answer WRONG.");
+							}
+				}	
+			}
 		}
-	}
 
