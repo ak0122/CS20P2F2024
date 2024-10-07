82aab1b607b1a7b133a7d2dd3d78a78f6c9ed2a4
1100102072 <1100102072@L815WD220009.edu.cbe.ab.ca>
2024-10-07 11:53:37 -0600

    11:53

--- chapter5/src/Mastery/PrimeNumberA.java
+++ chapter5/src/Mastery/PrimeNumberA.java
@@ -1,19 +1,34 @@
 package Mastery;
 
+import java.util.*;
+
 public class PrimeNumberA {
 
 	public static void main(String[] args)
 	{
 		//Declaration
-		int num,remainder,i;
+		int num,remainder=1,i=1;
 		
-		while ( remainder != 0) 
+		Scanner UserInput = new Scanner(System.in);
+		
+		System.out.print("Please Enter a Number: ");
+		num = UserInput.nextInt();
+		
+		do 
 		{
-			for (i=2; i<=7; i++)
-			{
-				
-			}
+			i=i+1;
+			remainder = num%i;
+		}while(remainder != 0 && i<=7);
+		
+		if(remainder != 0 || num == i) 
+		{
+			System.out.print("The Number is Prime");
 		}
+		else 
+		{
+			System.out.print("The Number is NOT Prime");
+		}
+		
 		
 	}
 }
