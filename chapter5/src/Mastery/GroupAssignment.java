a91656c9d1c412ae96c7279c3a0157e8dc65d807
1100102072 <1100102072@L815WD220009.edu.cbe.ab.ca>
2024-10-15 09:17:47 -0600

    9:17am 10/15/24

--- chapter5/src/Mastery/GroupAssignment.java
+++ chapter5/src/Mastery/GroupAssignment.java
@@ -22,6 +22,42 @@ public class GroupAssignment {
 
 	lastn = lastn.toLowerCase();
 	
+	//getting the first letter in last name:
+	char lastnameinitial = lastn.charAt(0);
+	switch (lastnameinitial) {
+	
+	case 'a': System.out.print("You are in Group 1"); break;
+	case 'b': System.out.print("You are in Group 1"); break;
+	case 'c': System.out.print("You are in Group 1"); break;
+	case 'd': System.out.print("You are in Group 1"); break;
+	case 'e': System.out.print("You are in Group 1"); break;
+	case 'f': System.out.print("You are in Group 1"); break;
+	case 'g': System.out.print("You are in Group 1"); break;
+	case 'h': System.out.print("You are in Group 1"); break;
+	case 'i': System.out.print("You are in Group 1"); break;
+	
+	case 'j': System.out.print("You are in Group 2"); break;
+	case 'k': System.out.print("You are in Group 2"); break;
+	case 'l': System.out.print("You are in Group 2"); break;
+	case 'm': System.out.print("You are in Group 2"); break;
+	case 'n': System.out.print("You are in Group 2"); break;
+	case 'o': System.out.print("You are in Group 2"); break;
+	case 'p': System.out.print("You are in Group 2"); break;
+	case 'q': System.out.print("You are in Group 2"); break;
+	case 'r': System.out.print("You are in Group 2"); break;
+	case 's': System.out.print("You are in Group 2"); break;
+	
+	case 't': System.out.print("You are in Group 3"); break;
+	case 'u': System.out.print("You are in Group 3"); break;
+	case 'v': System.out.print("You are in Group 3"); break;
+	case 'w': System.out.print("You are in Group 3"); break;
+	case 'x': System.out.print("You are in Group 3"); break;
+	case 'y': System.out.print("You are in Group 3"); break;
+	case 'z': System.out.print("You are in Group 3"); break;
+
+	}
+	
+
 	
 	
 	
