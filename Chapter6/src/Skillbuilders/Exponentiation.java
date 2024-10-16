09cf2c6e0fc0c5acf90e2f146e598481ab06ab81
1100102072 <1100102072@L815WD220009.edu.cbe.ab.ca>
2024-10-16 11:58:31 -0600

    asd

--- Chapter6/src/Skillbuilders/Exponentiation.java
+++ Chapter6/src/Skillbuilders/Exponentiation.java
@@ -2,4 +2,12 @@ package Skillbuilders;
 
 public class Exponentiation {
 
+	public static void powerOf(int num1,int num2)
+	{
+		int ans;
+		for (int i=1; i<=num2; i++)
+		{
+			ans = ans*num1;
+		}
+	}
 }
