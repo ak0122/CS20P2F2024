133a4e5ede57ff6ca907e86d0f7052258f79f016
1100102072 <1100102072@L815WD220009.edu.cbe.ab.ca>
2024-10-18 10:50:09 -0600

    1049

--- Chapter6/src/Skillbuilders/Exponentiation.java
+++ Chapter6/src/Skillbuilders/Exponentiation.java
@@ -1,13 +1,34 @@
 package Skillbuilders;
 
+import java.util.Scanner;
+
 public class Exponentiation {
 
-	public static void powerOf(int num1,int num2)
+	public static void powerOf(int base,int exp)
 	{
-		int ans;
-		for (int i=1; i<=num2; i++)
+		int ans=1;
+		for (int i=1; i<=exp; i++)
 		{
-			ans = ans*num1;
+			ans = ans*base;
 		}
+		System.out.print(ans);
+	}
+	
+	public static void main(String[] args)
+	{
+		int base,exp;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter the base: ");
+		base = input.nextInt();
+		
+		System.out.print("Please enter the exponent: ");
+		exp = input.nextInt();
+		
+		powerOf(base,exp);
+		
+		input.close();
+		
 	}
 }
