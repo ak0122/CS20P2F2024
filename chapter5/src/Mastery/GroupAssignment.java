82a4f0e672d2f110c5e2b75ee1522ff32970b6cd
1100102072 <1100102072@L815WD220009.edu.cbe.ab.ca>
2024-10-21 11:57:41 -0600

    1157

--- chapter5/src/Mastery/GroupAssignment.java
+++ chapter5/src/Mastery/GroupAssignment.java
@@ -12,51 +12,34 @@ public class GroupAssignment {
 		//Prepare for User Input
 		Scanner UserInput = new Scanner(System.in);
 		
-		//Get the First and Last name from User
+	//Get the First and Last name from User
 		
 	System.out.print("Enter your first name: ");
 	firstn = UserInput.next();
 	
 	System.out.print("Enter your last name: ");
 	lastn = UserInput.next();
-
-	lastn = lastn.toLowerCase();
+	
+	//Convert last name to lower case
+	String lastnlower = lastn.toLowerCase();
 	
 	UserInput.close();
 	
 	//getting the first letter in last name:
-	char lastnameinitial = lastn.charAt(0);
-	switch (lastnameinitial) {
-	
-	case 'a': System.out.print("You are in Group 1"); break;
-	case 'b': System.out.print("You are in Group 1"); break;
-	case 'c': System.out.print("You are in Group 1"); break;
-	case 'd': System.out.print("You are in Group 1"); break;
-	case 'e': System.out.print("You are in Group 1"); break;
-	case 'f': System.out.print("You are in Group 1"); break;
-	case 'g': System.out.print("You are in Group 1"); break;
-	case 'h': System.out.print("You are in Group 1"); break;
-	case 'i': System.out.print("You are in Group 1"); break;
-	
-	case 'j': System.out.print("You are in Group 2"); break;
-	case 'k': System.out.print("You are in Group 2"); break;
-	case 'l': System.out.print("You are in Group 2"); break;
-	case 'm': System.out.print("You are in Group 2"); break;
-	case 'n': System.out.print("You are in Group 2"); break;
-	case 'o': System.out.print("You are in Group 2"); break;
-	case 'p': System.out.print("You are in Group 2"); break;
-	case 'q': System.out.print("You are in Group 2"); break;
-	case 'r': System.out.print("You are in Group 2"); break;
-	case 's': System.out.print("You are in Group 2"); break;
-	
-	case 't': System.out.print("You are in Group 3"); break;
-	case 'u': System.out.print("You are in Group 3"); break;
-	case 'v': System.out.print("You are in Group 3"); break;
-	case 'w': System.out.print("You are in Group 3"); break;
-	case 'x': System.out.print("You are in Group 3"); break;
-	case 'y': System.out.print("You are in Group 3"); break;
-	case 'z': System.out.print("You are in Group 3"); break;
+	char lastnameinitial = lastnlower.charAt(0);
 
+	//If statement to arrange groups depending on initial
+	if (lastnameinitial >= 'a' && lastnameinitial <= 'i')
+	{
+		System.out.print(firstn + " "+ lastn + " is assigned to Group 1");
+	}
+	else if (lastnameinitial > 'i' && lastnameinitial <= 's')
+	{
+		System.out.print(firstn + " "+ lastn + " is assigned to Group 2");
+	}
+	else
+	{
+		System.out.print(firstn + " "+ lastn + " is assigned to Group 3");
 	}
 	}
 }
