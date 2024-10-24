5ee8794185e8da3f319448cfd64c08d16b8f4000
1100102072 <1100102072@L815WD220009.edu.cbe.ab.ca>
2024-10-24 10:15:35 -0600

    10:15am

--- chapter5/src/Mastery/GroupAssignment.java
+++ chapter5/src/Mastery/GroupAssignment.java
@@ -1,3 +1,16 @@
+/*
+
+Program: GroupAssignment.java          Last Date of this Revision: October 20, 2024
+
+Purpose: An application that assigns you to a group based on your last name.
+
+Author: Aryan K, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
 package Mastery;
 
 import java.util.Scanner;
@@ -42,4 +55,18 @@ public class GroupAssignment {
 		System.out.print(firstn + " "+ lastn + " is assigned to Group 3");
 	}
 	}
+	/* Screen Dump
+	 
+	Case 1:
+	Enter your first name: Aryan
+	Enter your last name: Kapoor
+	Aryan Kapoor is assigned to Group 2
+	
+	Case 2:
+	 Enter your first name: Justin
+	Enter your last name: Fung
+	Justin Fung is assigned to Group 1
+	 
+	 */
 }
+
