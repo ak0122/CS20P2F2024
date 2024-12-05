2fd700d3a65fb06bc0de65f2ef8fafde7e2814eb
1100102072 <1100102072@L815WD220009.edu.cbe.ab.ca>
2024-12-05 10:22:29 -0700

    asd

---
+++ Chapter9/src/Mastery/Palindrome.java
@@ -0,0 +1,20 @@
+package Mastery;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args)
+	{
+		
+		Scanner input = new Scanner(System.in);
+		
+		//enter a message
+		System.out.println("Please enter a message: ");
+		
+		String message = input.nextLine();
+		
+		message.toCharArray();
+		
+	}
+}
